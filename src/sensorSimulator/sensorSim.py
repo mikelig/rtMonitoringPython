01e669c872fe57c36155d76e5b81fe97234e166a
mikelig <mikelirazola@gmail.com>
2024-05-22 23:00:41 +0200

    [v2.0.0] Modified sensorSim docker. Added env vars. Added error handling to `.py` code

--- src/sensorSimulator/sensorSim.py
+++ src/sensorSimulator/sensorSim.py
@@ -2,11 +2,19 @@ import paho.mqtt.client as mqtt
 import json
 import time
 import random
+import os
+import logging
+
+logging.basicConfig(filename='testing.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
+logger = logging.getLogger(__name__)
 
 # Configuración del broker MQTT
-broker_address = "localhost"
-broker_port = 1883
-topic = "sensor_data"
+broker_address = os.environ.get('BROKER_ADD')
+broker_port = int(os.environ.get('BROKER_PORT'))
+topic = os.environ.get('MQTT_TOPIC')
+logging.info(f"HOST = {broker_address}  class: {broker_address.__class__}")
+logging.info(f"Port = {broker_port}  class: {broker_port.__class__}")
+logging.info(f"Topic = {topic}  class: {topic.__class__}")
 
 # Función para generar los datos aleatorios
 def generate_data():
@@ -19,13 +27,21 @@ def generate_data():
 # Función para publicar los datos en el broker MQTT
 def publish_data(client):
     while True:
-        data = generate_data()
-        client.publish(topic, data)
-        time.sleep(0.1)
+        try:
+            data = generate_data()
+            client.publish(topic, data)
+        except Exception as e:
+            logging.error(f"Error publishing: {e}")
+        finally:
+            time.sleep(0.1)
 
-# Conexión al broker MQTT
-client = mqtt.Client()
-client.connect(broker_address, broker_port)
-
-# Inicio de la publicación de datos
-publish_data(client)
+try:
+    # Conexión al broker MQTT
+    client = mqtt.Client()
+    client.connect(broker_address, broker_port)
+except Exception as e:
+    logging.error(f"Could not connect to broker: {e}")
+else:
+    # Inicio de la publicación de datos
+    logging.info("Connected. Starting publish")
+    publish_data(client)
